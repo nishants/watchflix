c0664460f717714e0ce350e991cf1b3008ef21a0
nishant <nishant.singh87@gmail.com>
2018-03-14 11:27:06 +0530

    Add movie mapper.

---
+++ app/models/movie-mapper.js
@@ -0,0 +1,19 @@
+const
+  Movie = require('./movie'),
+  mapRowToModle = ()=> {}
+
+module.exports = {
+  createMovie: (csvRow)=> {
+    return new Movie({
+      title       : csvRow[17],
+      movieId     : csvRow[3],
+      tagline     : csvRow[16],
+      homepage    : csvRow[2],
+      released    : csvRow[15]=== "Released",
+      runtime     : csvRow[13],
+      releaseDate : csvRow[11],
+      overview    : csvRow[7],
+      originalLanguage: csvRow[5],
+    }).save();
+  }
+}
---
+++ app/models/movie.js
@@ -0,0 +1,67 @@
+const
+  mongoose = require('mongoose'),
+  validator = require('validator');
+
+var MovieSchema = new mongoose.Schema({
+  title: {
+    type      : String,
+    required  : true,
+    minlength : 1,
+    trim      : true,
+  },
+  movieId: {
+    type    : Number,
+    required: true,
+    unique  : true,
+  },
+  tagline: {
+    type    : String,
+    trim    : true,
+  },
+  homepage: {
+    type    : String,
+    trim    : true,
+    validate: {
+      validator : validator.isURL,
+      message   : 'expected {VALUE} to be a valid URL'
+    }
+  },
+  released: {
+    type    : Boolean,
+    required: true,
+  },
+  runtime: {
+    type    : Number,
+    required: true
+  },
+  releaseDate: {
+    type: String,
+    trim: true,
+    validate: {
+      validator : (date)=>  /(\d{4})-(\d{2})-(\d{2})/.test(date),
+      message   : 'expected {VALUE} to be a valid YYYY-MM-DD string'
+    }
+  },
+  overview: {
+    type: String,
+    trim: true,
+  },
+  originalLanguage: {
+    type: String,
+    trim: true,
+  }
+});
+
+module.exports = mongoose.model('Movie', MovieSchema);
+
+/*
+* Fields
+* - homepage
+* - tagline
+* - status
+* - runtime
+* - release_date
+* - overview
+* - original_language
+* - keywords
+* */
