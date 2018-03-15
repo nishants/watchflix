b0f27726ddf1c27158873ae181c8514ca9e9fa93
nishant <nishant.singh87@gmail.com>
2018-03-15 17:26:00 +0530

    cleanup api

--- app/app.js
+++ app/app.js
@@ -22,32 +22,28 @@ app.get('/movies/user/:userId/search', (request, response)=> {
   const
     userId = request.params.userId,
     text   = request.query.text.split(','),
-    page   = Page.forRequest(request);
+    page   = Page.forRequest(request),
+
+    sendMovies = movies => response.send({page, movies}),
+    sendError  = error  => response.status(400).send({error: error.message});
 
   SearchMovies.searchForUser(userId, text, page).then(movies => {
-    if(movies.length) {
-      response.send({page, movies});
-    }else{
-      SearchMovies.searchForText(text, page).then(movies => {
-        response.send({page, movies});
-      }).catch((error)=> {
-        response.status(500).send({error: error.message});
-      });
-    }
-  }).catch((error)=> {
-    response.status(500).send({error: error.message});
-  });
+    movies.length ?
+      sendMovies(movies)
+      : SearchMovies.searchForText(text, page).then(sendMovies).catch(sendError);
+  }).catch(sendError);
 });
 
 app.get('/movies/users', (request, response)=> {
   const
     page = Page.forRequest(request),
+    sendPreferences = userPreferences => {
+      response.send({page, userPreferences});
+    };
     onError = (error)=> response.status(400).send({error: error.message});
 
   User.getAllByPage(page).then(users => {
-    SearchMovies.getPreferencesFor(users).then(userPreferences => {
-      response.send({page, userPreferences});
-    }).catch(onError);
+    SearchMovies.getPreferencesFor(users).then(sendPreferences).catch(onError);
   }).catch(onError);
 });
 
