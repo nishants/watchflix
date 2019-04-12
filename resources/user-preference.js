afc610c3bb3c3cd6dea240d93fc75e7fd7f7e992
nishant <nishant.singh87@gmail.com>
2019-04-12 14:34:37 +0530

    Add full seed data.

---
+++ resources/user-preference.js
@@ -0,0 +1,97 @@
+[
+  {
+    "100": {
+      "preferred_languages": ["English"],
+      "favourite_actors": [      
+        "Kate Winslet"       
+      ],
+      "favourite_directors": [
+      ]
+    }
+  },
+  {
+    "101": {
+      "preferred_languages": [
+        "English"
+      ],
+      "favourite_actors": [
+        "Tom Hanks"
+      ],
+      "favourite_directors": [
+      ]
+    }
+  },
+  {
+    "102": {
+      "preferred_languages": [
+        "English"
+      ],
+      "favourite_actors": [
+      ],
+      "favourite_directors": [
+        "Steven Spielberg",
+      ]
+    }
+  },
+
+  {
+    "103": {
+      "preferred_languages": [
+        "Spanish"
+      ],
+      "favourite_actors": [],
+      "favourite_directors": []
+    }
+  },
+  {
+    "104": {
+      "preferred_languages": [
+        "English"
+      ],
+      "favourite_actors": [
+        "Anthony Hopkins",
+        "Adam Sandler",
+        "Bruce Willis"
+      ],
+      "favourite_directors": [
+        "Nora Ephron",
+        "Oliver Stone"
+      ]
+    }
+  },
+  {
+    "105": {
+      "preferred_languages": [
+        "Spanish"
+      ],
+      "favourite_actors": [
+        "Anthony Hopkins",
+        "Bárbara Goenaga",
+        "Tenoch Huerta"
+      ],
+      "favourite_directors": [
+        "Amat Escalante",
+        "Robert Rodriguez"
+      ]
+    }
+  },
+  {
+    "106": {
+      "preferred_languages": [
+        "English",
+        "Spanish"
+      ],
+      "favourite_actors": [
+        "Brad Pitt",
+        "Robert Downey Jr.",
+        "Jennifer Lawrence",
+        "Johnny Depp"
+      ],
+      "favourite_directors": [
+        "Steven Spielberg",
+        "Martin Scorsese",
+        "Ridley Scott"
+      ]
+    }
+  }
+]
